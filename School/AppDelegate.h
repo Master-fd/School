5b904345907ff5e902036653b55885c3a72aa3bf
asus <asus@asusdeMac-Pro.local>
2016-03-05 14:37:18 +0800

    Initial Commit

---
+++ School/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  School
+//
+//  Created by asus on 16/3/5.
+//  Copyright (c) 2016年 asus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ School/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  School
+//
+//  Created by asus on 16/3/5.
+//  Copyright (c) 2016年 asus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
